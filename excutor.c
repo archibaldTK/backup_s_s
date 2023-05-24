afac21f0e42b1b22d2a2c639f810d0ad4d0e97cf
TsitsiNdhlovu <tsitsindhlovu211@gmail.com>
2023-05-24 12:22:47 -0700

    excutor.c

---
+++ excutor.c
@@ -0,0 +1,36 @@
+#include "sim_shell.h"/**
+* builtin_execute - function to select which builtin is appropriate
+* code by ArchibaldTK & TsistiN
+* @tokens: user input
+* Return: Parsed input
+*/
+int builtin_execute(char **tokens)
+{
+	int status;
+	unsigned int length;
+	unsigned int num;
+	unsigned int x;	built_s builtin[] = {
+		{"exit", shell_exit},
+		{"env", shell_env},
+		{NULL, NULL}
+	};	if (tokens[0] == NULL)
+		return (1);	length = _strlen(tokens[0]);	num = shell_num_builtins(builtin);
+	for (x = 0; x < num; x++)
+	{
+		if (_strcmp(tokens[0], builtin[x].name, length) == 0)
+		{
+			status = (builtin[x].p)();
+			return (status);
+		}
+	}
+	return (1);
+}/**
+* shell_num_builtins - function to determine how many builtin exist
+* @builtin: reciever
+* Return: Possible options
+*/int shell_num_builtins(built_s builtin[])
+{
+	unsigned int x;	x = 0;
+	while (builtin[x].name != NULL)
+		x++;	return (x);
+}
