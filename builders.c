ca9bcd9095997c37dac19c527af19135b9e6639e
TsitsiNdhlovu <tsitsindhlovu211@gmail.com>
2023-05-25 00:39:22 -0700

    simple_shell

---
+++ builders.c
@@ -0,0 +1,35 @@
+#include "sim_shell.h"
+
+/**
+* shell_exit - similar functionality to exit command
+* code by ArchibaldTK & TsistiN
+* Return: Nothing
+*/
+
+	int shell_exit(void)
+	{
+		return (-1);
+	}
+
+
+/**
+* shell_env - function that prints environment
+* code by ArchibaldTK & TsistiN
+* Return: none
+*/
+
+int shell_env(void)
+{
+	unsigned int x;
+
+	x = 0;
+	while (environ[i] != NULL)
+	{
+		write(STDOUT_FILENO, environ[x], _strlen(environ[x]));
+		write(STDOUT_FILENO, "\n", 1);
+		x++;
+	}
+	return (0);
+}
+Footer
+
